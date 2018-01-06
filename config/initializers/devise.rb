593fbccce59856623c4def1056c8825c9bfc31d2
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-01-06 14:33:50 +0900

    add facebook & twitter

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -253,6 +253,8 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
+  config.omniauth :facebook, 'App ID', 'App Secret'
+  config.omniauth :twitter, 'API key', 'API secret'
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
